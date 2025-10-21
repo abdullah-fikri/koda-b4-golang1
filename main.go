0e28c362f0e068fc71ac9c9bbcd29a1dd3597d24
abdullah-fikri <abdullahfikri981@gmail.com>
2025-10-21 14:55:44 +0700

    feat: added function circleWide for calc wide circle

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ func circleAround(p int) float64 {
 	return result
 }
 
+func circleWide(p int)float64{
+	phi := 3.14
+	result := 2 * phi * float64(p)
+	return result
+}
+
 
 func main() {
 	fmt.Println(circleAround(10))
+	fmt.Println(circleWide(10))
 }
